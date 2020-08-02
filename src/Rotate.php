3899a4659daeb1313eb475829850a866dc0c685a
Austin Adams <adamat01@pfw.edu>
2020-08-02 00:54:16 -0400

    Rotate Function

---
+++ src/Rotate.php
@@ -0,0 +1,19 @@
+<?php
+	function RotateArray($rotations) {
+		$contentJson = file_get_contents('../assets/rotate.json');
+		$arrayJson = json_decode($contentJson, true);
+		echo "Initial Array: "; 
+		print_r($arrayJson);
+		echo '<br/>';
+		$addToEnd = "";
+		for ($i=$rotations; 1 <= $i; $i--) {
+			$addToEnd = $arrayJson[0];
+			array_splice($arrayJson, 0, 1);
+			array_push($arrayJson, $addToEnd);
+		}
+		return $arrayJson;
+	}
+$lsArray = RotateArray(4);
+echo "Left Shifted Array: ";
+print_r($lsArray);
+?>
